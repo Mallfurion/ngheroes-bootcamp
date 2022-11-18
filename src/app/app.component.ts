13c2d2b1b9bb5a6308df458cf729546d5d96fc88
Florin Tomozei <florin.scouts@gmail.com>
2022-11-18 17:07:03 +0200

    add initial mocked data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { mockedData } from './data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngheroes-bootcamp';
+  title = 'JS Heroes Bootcamp';
+  data = mockedData;
+
+  openNewTab(url: string) {
+    window.open(url, '_blank');
+  }
 }
---
+++ src/app/data.ts
@@ -0,0 +1,5 @@
+export const mockedData = [
+  { id: 1, name: 'angular', url: 'https://github.com/angular/angular', description: 'Lorem Ipsum Angular' },
+  { id: 2, name: 'vue', url: 'https://github.com/vuejs/core', description: 'Lorem Ipsum Vue' },
+  { id: 3, name: 'react', url: 'https://github.com/facebook/react', description: 'Lorem Ipsum React' }
+];
