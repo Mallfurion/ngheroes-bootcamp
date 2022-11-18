20c3ebc140b398270bfc938467d3994270e815f7
Florin Tomozei <florin.scouts@gmail.com>
2022-11-18 22:56:53 +0200

    fix data

--- src/app/data.ts
+++ src/app/data.ts
@@ -3,9 +3,22 @@ export const mockedData = [
     id: 1,
     name: 'angular',
     url: 'https://github.com/angular/angular',
+    owner: { login: 'test' },
     description:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta totam repellendus ut alias quo laboriosam sed iste dolores, quasi harum exercitationem saepe, voluptate deleniti in dolore! Est a quia molestiae? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta totam repellendus ut alias quo laboriosam sed iste dolores, quasi harum exercitationem saepe, voluptate deleniti in dolore! Est a quia molestiae? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta totam repellendus ut alias quo laboriosam sed iste dolores, quasi harum exercitationem saepe, voluptate deleniti in dolore! Est a quia molestiae?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta totam repellendus ut alias quo laboriosam sed iste dolores, quasi harum exercitationem saepe, voluptate deleniti in dolore! Est a quia molestiae?'
   },
-  { id: 2, name: 'vue', url: 'https://github.com/vuejs/core', description: 'Lorem Ipsum Vue' },
-  { id: 3, name: 'react', url: 'https://github.com/facebook/react', description: 'Lorem Ipsum React' }
+  {
+    id: 2,
+    name: 'vue',
+    url: 'https://github.com/vuejs/core',
+    description: 'Lorem Ipsum Vue',
+    owner: { login: 'test' }
+  },
+  {
+    id: 3,
+    name: 'react',
+    url: 'https://github.com/facebook/react',
+    description: 'Lorem Ipsum React',
+    owner: { login: 'test' }
+  }
 ];
